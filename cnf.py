f2e835ff59fc4b07a50ebc2a67ce3e416212659d
j129008 <j129008@gmail.com>
2015-12-18 19:59:22 +0800

    postfix and solve bug

--- cnf.py
+++ cnf.py
@@ -1,31 +1,29 @@
 
-ops = { 'iff': 1, 'imp': 2, 'or': 3, 'and': 4, 'not': 5 }
+ops = { 'iff': 1, 'imp': 2, 'or': 3, 'and': 4, 'not': 5, '(': 0, ')':6 }
 opStack = []
 postfix = []
 
 text = raw_input("input your logic: ")
 tokens = text.split(" ")
-print tokens
 
 while len(tokens) > 0 :
     token = tokens.pop(0)
-    if token not in ops :  # check op or symbol or ( or )
-        if token == '(':
-            opStack = list(token) + opStack
-        elif token == ')':
-            while opStack[0] != '(' :
-                postfix.append(opStack.pop(0))
-            opStack.pop(0)
-        else:
-            postfix.append(token)
-    elif len( opStack ) == 0 : # check op stack empty
+    if len( opStack ) == 0 : # check op stack empty
         opStack.append(token)
-    else:
+    elif token == '(':
+        opStack = ['('] + opStack
+    elif token == ')':
+        while opStack[0] != '(' :
+            postfix.append(opStack.pop(0))
+        opStack.pop(0)
+    elif token in ops:
         while ops[token] <= ops[opStack[0]]:
             postfix.append(opStack.pop(0))
             if len(opStack) == 0 :
                 break
         opStack = [token] + opStack
+    else:
+        postfix.append(token)
 
 
 while len(opStack) > 0 :
