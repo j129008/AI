9b5009ba93aab01a634ef3557d45095cf6993dee
j129008 <j129008@gmail.com>
2015-12-19 22:37:25 +0800

    split function

--- cnf.cpp
+++ cnf.cpp
@@ -11,27 +11,62 @@ using namespace std;
 using namespace boost;
 
 map<string, int> ops;
-vector<string> parseTree;
 void init();
+
+template<typename T>
+void printVec(vector<T>);
+vector<string> toToken(string);
+vector<string> toPostfix(vector<string>);
 
 
 int main(int argc, char const* argv[]){
+    vector<string> tokens;
+    vector<string> postfix;
+
+    // file I/O & ops
     init();
     ifstream in(argv[1]);
     ofstream out(argv[2]);
+
     string line;
     while(getline(in, line)){
-        replace(line, "(", " ( ");
-        replace(line, ")", " ) ");
-        char_separator<char> sp(" ");
-        tokenizer< char_separator<char> > tokens(line, sp);
-        BOOST_FOREACH (const string& t, tokens) {
-            parseTree.push_back(t);
-        }
+        tokens = toToken(line);
+        postfix = toPostfix(tokens);
     }
-
+    printVec(postfix);
 
     return 0;
+}
+
+template<typename T>
+void printVec(vector<T> vec){
+    BOOST_FOREACH (const T& ele, vec) {
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+}
+
+vector<string> toToken(string line){
+    replace(line, "(", " ( ");
+    replace(line, ")", " ) ");
+    vector<string> inputTokens;
+    char_separator<char> sp(" ");
+    tokenizer< char_separator<char> > tokens(line, sp);
+    BOOST_FOREACH (const string& t, tokens) {
+        inputTokens.push_back(t);
+    }
+    return inputTokens;
+}
+
+vector<string> toPostfix(vector<string> tokens){
+    while(tokens.size()){
+        string token = tokens[0];
+        tokens.erase(tokens.begin());
+        //if(token == "("){
+
+        //}
+    }
+    return tokens;
 }
 
 void init(){
